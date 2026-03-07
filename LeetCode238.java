934775a6754b300e454a41294135a9950c288a47
dev-pa5an <pasanfernando24@gmail.com>
2026-03-07 15:30:23 +0530

    optimmal solution

--- LeetCode238.java
+++ LeetCode238.java
@@ -1,18 +1,16 @@
 public class LeetCode238 {
     public int[] productExceptSelf(int[] nums) {
-        int[] fSum = new int[nums.length];
-        int[] rSum = new int[nums.length];
+        int n = nums.length;
+        int[] ans = new int[n];
 
-        fSum[0] = 1;
-        rSum[nums.length-1] = 1;
-
+        ans[0] = 1;
         for (int i = 1; i < nums.length; i++) {
-            fSum[i] = fSum[i-1] * nums[i-1];
-            rSum[nums.length-1-i] = rSum[nums.length-i] * nums[nums.length - i];
+            ans[i] = ans[i-1] * nums[i-1];
         }
-        int[] ans = new int[nums.length];
-        for (int j = 0; j < nums.length; j++) {
-            ans[j] = fSum[j] * rSum[j];
+        int suffix = 1;
+        for (int i = n - 1; i >= 0;  i--) {
+            ans[i] *= suffix;
+            suffix *= nums[i]; 
         }
         return ans;
     }
