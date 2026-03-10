cd989e68ce84c8b88a168710abb472dc5d778ba5
dev-pa5an <pasanfernando24@gmail.com>
2026-03-10 23:46:30 +0530

    Leetcode 128 : Longest consecutive sequence: O(n)

---
+++ LeetCode128.java
@@ -0,0 +1,20 @@
+import java.util.HashSet;
+import java.util.Set;
+
+public class LeetCode128 {
+    public int longestConsecutive(int[] nums) {
+        Set<Integer> set = new HashSet<>();
+        for (int n : nums) set.add(n);
+
+        int longest = 0;
+
+        for (int n : set) {
+            if (!set.contains(n - 1)) {
+                int length = 1;
+                while (set.contains(n + length)) length++;
+                longest = Math.max(longest, length);
+            }
+        }
+        return longest;
+    }
+}
